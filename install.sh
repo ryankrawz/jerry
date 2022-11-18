9410ebc3c55caeb380f7990d11e22fcb823158ee
Ryan Krawczyk <rkrawczy@USMBP16rkrawczy.local>
2022-11-18 16:23:40 -0500

    Brew install called in either case

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ if ! [ -x $(command -v brew) ]
 then
 printf "The required library Pillow has dependencies that should be installed via Homebrew. Installing now...\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-brew install libjpeg libtiff little-cms2 openjpeg webp
 fi
 
+brew install libjpeg libtiff little-cms2 openjpeg webp
 pip3 install -r requirements.txt
